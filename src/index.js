2cb9b26072103966af01b702c15eb2626bb82ff6
DANELLE19 <debruyndanelle@gmail.com>
2023-12-29 19:19:03 +0200

    Updated index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "bf3ccbcobafd70tb80f0400b80e3e34f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#seach-form-input");
-    let cityElement = document.querySelector("city");
-    cityElement.innerHTML = searchInput.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nababeep");
